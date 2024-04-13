2b196bbad4963574af108a11492a4a3b310e828b
Inosuke Hashibira <99578162+bilbilaki@users.noreply.github.com>
2024-04-14 01:04:21 +0330

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,10 +5,10 @@ module.exports = async (req, res) => {
     const path = req.url.replace('/api/proxy', '');
 
     const response = await axios({
-      url: `https://api.cloudflare.com/client/v4/accounts/${process.env.REACT_APP_ACCOUNT_ID}/ai/run${path}`,
+      url: `https://api.cloudflare.com/client/v4/accounts/${process.env.6d0eda71212a9ce25a5e47b40f45b5d3}/ai/run${path}`,
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+        'Authorization': `Bearer ${process.env.surnanqPoo2llV2NTCzRXjTIgj_r3V3kanNEWv_B}`,
       },
       data: req.body,
       timeout: 30000,
